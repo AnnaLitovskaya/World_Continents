190bf6ae8c9374b1e51d060dc2cffff1d2c7d05e
Anna Litovskaya <Litovskaya09@gmail.com>
2021-03-03 17:33:55 -0500

    backend done

---
+++ Server/api/routes.js
@@ -0,0 +1,45 @@
+const { client } = require('../db/db');
+const router = require('express').Router();
+
+router.get('/continents', async (req, res, next) => {
+  try {
+    const continents = await client.query('SELECT * FROM continent;');
+    res.send(continents.rows);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+router.get('/continents/country/:id', async (req, res, next) => {
+  try {
+    const country = await client.query(
+      `
+    SELECT country.name AS "country", continent.name AS "continent" FROM continent
+    JOIN country ON country.continent_id = continent.id
+    WHERE continent.id = $1
+  `,
+      [req.params.id]
+    );
+    res.send(country.rows);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+router.get('/continents/fact/:id', async (req, res, next) => {
+  try {
+    const fact = await client.query(
+      `
+    SELECT data.fact AS "fact", continent.name AS "continent" FROM continent
+    JOIN data ON data.continent_id = continent.id
+    WHERE continent.id = $1
+  `,
+      [req.params.id]
+    );
+    res.send(fact.rows);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+module.exports = router;
---
+++ Server/server.js
@@ -0,0 +1,27 @@
+const { client, syncAndSeed } = require('./db/db');
+const path = require('path');
+const express = require('express');
+const app = express();
+const router = require('./api/routes');
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.use('/api', router);
+
+app.get('/', (req, res, next) =>
+  res.sendFile(path.join(__dirname, 'index.html'))
+);
+
+const setUp = async () => {
+  try {
+    await client.connect();
+    await syncAndSeed();
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log(`listening on port ${port}`));
+    console.log(`connected to database`);
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+setUp();
